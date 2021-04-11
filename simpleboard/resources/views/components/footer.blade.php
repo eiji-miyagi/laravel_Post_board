b5649ce94ff17e3a83e815a8d4f859576526f9a4
宮城　永次 <miyagieiji@miyagieijinoMacBook-Pro.local>
2021-04-11 17:52:26 +0900

    再アップロード前

---
+++ simpleboard/resources/views/components/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="py-3 bg-light fixed-bottom">
+    <div class="container">
+        <span class="text-muted small">Simple Board by Laravel & Bootstrap　4</span>
+
+    </div>
+</footer>
---
+++ simpleboard/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|これは、アプリケーションのWebルートを登録できる場所です。これらのルートは、
+「web」ミドルウェアグループを含むグループ内のRouteServiceProviderによってロードされます。
+　今、素晴らしいものを作成してください！
+*/
+// URL末尾/でウェルカムのビューを基本表示
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/', 'PostController@index');
+// URL末尾postsでPostControllerのリソース実行/
+Route::resource('posts', 'PostController');
+
+if (env('APP_ENV')=== 'local'){
+    URL::forceScheme('http');
+}
