3eefe08bf2c43472d9504307139f2f5d1056de11
宮城　永次 <miyagieiji@miyagieijinoMacBook-Pro.local>
2021-04-21 20:12:51 +0900

    post_bord入れ替え

--- simpleboard/resources/views/components/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="py-3 bg-light fixed-bottom">
-    <div class="container">
-        <span class="text-muted small">Simple Board by Laravel & Bootstrap　4</span>
-
-    </div>
-</footer>
